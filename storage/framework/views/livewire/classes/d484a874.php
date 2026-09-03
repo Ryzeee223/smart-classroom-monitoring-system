64013ad4f00e7a39ec2bd1d9af9af9f82893adda
Ryzee <palcerontobia@gmail.com>
2026-09-03 23:41:01 +0800

    testing

---
+++ storage/framework/views/livewire/classes/d484a874.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+return new class extends Component
+{
+    //
+
+    protected function view($data = [])
+    {
+        return app('view')->file('/Volumes/shared/capstone project/backups/emonitor 3rd phase copy/storage/framework/views/livewire/views/d484a874.blade.php', $data);
+    }
+};
